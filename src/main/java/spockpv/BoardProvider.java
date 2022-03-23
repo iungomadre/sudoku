801b330ba55d1c192ea580a3914a5516452dc6c4
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 22:21:31 +0100

    Docs: Document BoardProvider

--- src/main/java/spockpv/BoardProvider.java
+++ src/main/java/spockpv/BoardProvider.java
@@ -1,6 +1,16 @@
 package spockpv;
 
+/**
+ * Represents board provider whose task is to provide already-built Board object
+ * from whatever source it works with
+ * 
+ * @author  Filip Wiśniewski
+ */
 public interface BoardProvider {
-    // Creates and returns game board
-    public Board createBoard();
+  /**
+   * Creates and returns new Board object with parameters specified by
+   * derivative class
+   * @return already-built board
+   */
+  public Board createBoard();
 }
