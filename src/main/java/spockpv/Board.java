aef2c97e15ba6e12dc8e6351dc6a9c72f2f62f4b
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 22:19:06 +0100

    Docs: Document Board

--- src/main/java/spockpv/Board.java
+++ src/main/java/spockpv/Board.java
@@ -1,90 +1,128 @@
 package spockpv;
 
+/**
+ * Represents game board and its state
+ *
+ * @author  Filip Wiśniewski
+ */
 public class Board implements Cloneable {
-    /* Represents Sudoku board state */
 
-    private Character[][] boardState_;
+  private Character[][] boardState_;
 
-    public Board(Character[][] initialBoardState) {
-        boardState_ = initialBoardState;
+  /**
+   * Parameter constructor
+   *
+   * @param initialBoardState array representing board[at_x][at_y]
+   */
+  public Board(Character[][] initialBoardState) {
+    boardState_ = initialBoardState;
+  }
+
+  /**
+   * Sets value to value at certain coordinates
+   * @param to  what to change value for
+   * @param at  where to change it
+   */
+  public void setCellValue(Character to, Coordinates at) {
+    boardState_[at.x()][at.y()] = to;
+  }
+
+  /**
+   * Gets value at coordinates specified
+   * @param from
+   * @return  character at coordinates passed
+   */
+  public Character getCellValue(Coordinates from) {
+    return boardState_[from.x()][from.y()];
+  }
+
+  /**
+   * @return board width
+   */
+  public Integer width() { return boardState_.length; }
+
+  /**
+   * @return board height
+   */
+  public Integer height() { return boardState_[0].length; }
+
+  /**
+   * Implementation of Java's equals operator
+   * Compares values, not addresses
+   * @param o object to compare with
+   * @return is equal
+   */
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof Board)) {
+      return false;
     }
 
-    public void setCellValue(Character to, Coordinates at) {
-        boardState_[at.x()][at.y()] = to;
+    Board compared = (Board)o;
+    for (int i = 0; i < compared.boardState_.length; i++) {
+      for (int j = 0; j < compared.boardState_[i].length; j++) {
+        if (this.boardState_[i][j] != compared.boardState_[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * @return deep copy of object
+   * Requires casting in order to get proper type
+   */
+  @Override
+  public Object clone() {
+
+    Character[][] newBoardState =
+        new Character[this.boardState_.length][this.boardState_[0].length];
+
+    for (Integer i = 0; i < this.boardState_.length; i++) {
+      for (Integer j = 0; j < this.boardState_[i].length; j++) {
+        newBoardState[i][j] = this.boardState_[i][j];
+      }
     }
 
-    public Character getCellValue(Coordinates from) {
-        return boardState_[from.x()][from.y()];
-    }
+    return new Board(newBoardState);
+  }
 
-    public Integer width() {
-        return boardState_.length;
-    }
+  /**
+   * Counts sum of digits in every row
+   * @return array of sums in every row
+   */
+  public Integer[] countRowSums() {
+    Integer[] sums = new Integer[this.height()];
+    Integer sum;
 
-    public Integer height() {
-        return boardState_[0].length;
+    for (int y = 0; y < this.height(); y++) {
+      sum = 0;
+      for (int x = 0; x < this.width(); x++) {
+        sum = sum + Character.getNumericValue(boardState_[x][y]);
+      }
+      sums[y] = sum;
     }
-    
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof Board)) {
-            return false;
-        }
-        
-        Board compared = (Board)o;
-        for (int i = 0; i < compared.boardState_.length; i++) {
-            for (int j = 0; j < compared.boardState_[i].length; j++) {
-                if (this.boardState_[i][j] != compared.boardState_[i][j]){
-                    return false;
-                }
-            }
-        }
-        return true;
+    return sums;
+  }
+
+  /**
+   * Counts sum of digits in every column
+   * @return array of sums in every column
+   */
+  public Integer[] countColumnSums() {
+    Integer[] sums = new Integer[this.width()];
+    Integer sum;
+
+    for (int x = 0; x < this.width(); x++) {
+      sum = 0;
+      for (int y = 0; y < this.height(); y++) {
+        sum = sum + Character.getNumericValue(boardState_[x][y]);
+      }
+      sums[x] = sum;
     }
-    
-    @Override
-    public Object clone() {
-        
-        Character[][] newBoardState = new Character[this.boardState_.length][this.boardState_[0].length];
-        
-        for (Integer i = 0; i < this.boardState_.length; i++) {
-            for (Integer j = 0; j < this.boardState_[i].length; j++) {
-                newBoardState[i][j] = this.boardState_[i][j];
-            }
-        }
-        
-        return new Board(newBoardState);
-    }
-    
-        // counts sum of numbers in every row
-        public Integer[] countRowSums() {
-            Integer[] sums = new Integer[this.height()];
-            Integer sum;
-    
-            for (int y = 0; y < this.height(); y++) {
-                sum = 0;
-                for (int x = 0; x < this.width(); x++) {
-                    sum = sum + Character.getNumericValue(boardState_[x][y]);
-                }
-                sums[y] = sum;
-            }
-            return sums;
-        }
-    
-        // counts sum of numbers in every column
-        public Integer[] countColumnSums() {
-            Integer[] sums = new Integer[this.width()];
-            Integer sum;
-    
-            for (int x = 0; x < this.width(); x++) {
-                sum = 0;
-                for (int y = 0; y < this.height(); y++) {
-                    sum = sum + Character.getNumericValue(boardState_[x][y]);
-                }
-                sums[x] = sum;
-            }
-            return sums;
-        }
+    return sums;
+  }
 }
