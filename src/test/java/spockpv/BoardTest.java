cfeb5f4bed6b27a528a145dcd7bd5a3fe98f6a60
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-24 00:51:29 +0100

    Style: Change coding standard in test files

--- src/test/java/spockpv/BoardTest.java
+++ src/test/java/spockpv/BoardTest.java
@@ -1,144 +1,137 @@
 package spockpv;
 
-import org.junit.Test;
-import static org.junit.Assert.assertNotSame;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotSame;
+
+import org.junit.Test;
 
 public class BoardTest {
 
-    final Character[][] initialBoardState = {
-        { '0', '1' },
-        { '2', '0' },
-        { '1', '3' }
-    };
-    final Character[][] differentInitialBoardState = {
-        { 'x', 'y' },
-        { 'x', 'y' },
-        { 'x', 'y' }
-    };
+  final Character[][] initialBoardState = {{'0', '1'}, {'2', '0'}, {'1', '3'}};
+  final Character[][] differentInitialBoardState = {
+      {'x', 'y'}, {'x', 'y'}, {'x', 'y'}};
 
-    private final Board underTest = new Board(initialBoardState);
-    private final Board differentUnderTest = new Board(differentInitialBoardState);
+  private final Board underTest = new Board(initialBoardState);
+  private final Board differentUnderTest =
+      new Board(differentInitialBoardState);
 
-    @Test
-    public void testGetCellValue() {
-        assertThat(underTest.getCellValue(new Coordinates(0, 0))).isEqualTo('0');
-        assertThat(underTest.getCellValue(new Coordinates(1, 0))).isEqualTo('2');
-        assertThat(underTest.getCellValue(new Coordinates(0, 1))).isEqualTo('1');
-        assertThat(underTest.getCellValue(new Coordinates(1, 1))).isEqualTo('0');
-    }
+  @Test
+  public void testGetCellValue() {
+    assertThat(underTest.getCellValue(new Coordinates(0, 0))).isEqualTo('0');
+    assertThat(underTest.getCellValue(new Coordinates(1, 0))).isEqualTo('2');
+    assertThat(underTest.getCellValue(new Coordinates(0, 1))).isEqualTo('1');
+    assertThat(underTest.getCellValue(new Coordinates(1, 1))).isEqualTo('0');
+  }
 
-    @Test
-    public void testSetCellValue() {
-        // given
-        Character[][] localBoardState = {
-            { '2', '0' },
-            { '3', '0' },
-            { '4', '0' }
-        };
-        Board localUnderTest = new Board(localBoardState);
+  @Test
+  public void testSetCellValue() {
+    // given
+    Character[][] localBoardState = {{'2', '0'}, {'3', '0'}, {'4', '0'}};
+    Board localUnderTest = new Board(localBoardState);
 
-        Coordinates testedCoordinates = new Coordinates(0, 0);
-        Character testedValue = 'x';
+    Coordinates testedCoordinates = new Coordinates(0, 0);
+    Character testedValue = 'x';
 
-        assertThat(localUnderTest.getCellValue(testedCoordinates)).isNotEqualTo(testedValue);
-        
-        // when
-        localUnderTest.setCellValue(testedValue, testedCoordinates);
-        
-        // then
-        assertThat(localUnderTest.getCellValue(testedCoordinates)).isEqualTo(testedValue);
-    }
+    assertThat(localUnderTest.getCellValue(testedCoordinates))
+        .isNotEqualTo(testedValue);
 
-    @Test
-    public void testWidth() {
-        // given
-        Integer expected = 3;
+    // when
+    localUnderTest.setCellValue(testedValue, testedCoordinates);
 
-        // when
-        Integer actual = underTest.width();
+    // then
+    assertThat(localUnderTest.getCellValue(testedCoordinates))
+        .isEqualTo(testedValue);
+  }
 
-        // then
-        assertThat(actual).isEqualTo(expected);
-    }
+  @Test
+  public void testWidth() {
+    // given
+    Integer expected = 3;
 
-    @Test
-    public void testHeight() {
-        // given
-        Integer expected = 2;
+    // when
+    Integer actual = underTest.width();
 
-        // when
-        Integer actual = underTest.height();
+    // then
+    assertThat(actual).isEqualTo(expected);
+  }
 
-        // then
-        assertThat(actual).isEqualTo(expected);
-    }
+  @Test
+  public void testHeight() {
+    // given
+    Integer expected = 2;
 
-    @Test
-    public void testIsEqual() {
-        // given
-        Board first = new Board(initialBoardState);
-        Board second = new Board(initialBoardState);
+    // when
+    Integer actual = underTest.height();
 
-        // then
-        assertThat(first).isEqualTo(second);
-    }
+    // then
+    assertThat(actual).isEqualTo(expected);
+  }
 
-    @Test
-    public void testIsNotEqual() {
-        assertThat(underTest).isNotEqualTo(differentUnderTest);
-    }
+  @Test
+  public void testIsEqual() {
+    // given
+    Board first = new Board(initialBoardState);
+    Board second = new Board(initialBoardState);
 
-    @Test
-    public void testIsEqualSameObject() {
-        assertThat(underTest).isEqualTo(underTest);
-    }
+    // then
+    assertThat(first).isEqualTo(second);
+  }
 
-    @Test
-    public void testIsNotEqualDifferentObject() {
-        // given 
-        Integer notBoard = 2;
+  @Test
+  public void testIsNotEqual() {
+    assertThat(underTest).isNotEqualTo(differentUnderTest);
+  }
 
-        // then
-        assertThat(underTest).isNotEqualTo(notBoard);
-    }
+  @Test
+  public void testIsEqualSameObject() {
+    assertThat(underTest).isEqualTo(underTest);
+  }
 
-    @Test
-    public void testCloneIsNotSame() {
-        // given
-        Board clone = (Board)underTest.clone();
+  @Test
+  public void testIsNotEqualDifferentObject() {
+    // given
+    Integer notBoard = 2;
 
-        // then
-        assertNotSame(underTest, clone);
-    }
+    // then
+    assertThat(underTest).isNotEqualTo(notBoard);
+  }
 
-    @Test
-    public void testCloneEquals() {
-        // given
-        Board clone = (Board)underTest.clone();
+  @Test
+  public void testCloneIsNotSame() {
+    // given
+    Board clone = (Board)underTest.clone();
 
-        // then
-        assertThat(underTest).isEqualTo(clone);
-    }
-    
-    @Test
-    public void testCountRowSums() {
-        // given
-        Integer[] rowSums = underTest.countRowSums();
+    // then
+    assertNotSame(underTest, clone);
+  }
 
-        // then
-        assertThat(rowSums[0]).isEqualTo(3);
-        assertThat(rowSums[1]).isEqualTo(4);
-    }
+  @Test
+  public void testCloneEquals() {
+    // given
+    Board clone = (Board)underTest.clone();
 
-    @Test
-    public void testCountColumnSums() {
-        // given
-        Integer[] columnSums = underTest.countColumnSums();
+    // then
+    assertThat(underTest).isEqualTo(clone);
+  }
 
-        // then
-        assertThat(columnSums[0]).isEqualTo(1);
-        assertThat(columnSums[1]).isEqualTo(2);
-        assertThat(columnSums[2]).isEqualTo(4);
-    }
+  @Test
+  public void testCountRowSums() {
+    // given
+    Integer[] rowSums = underTest.countRowSums();
+
+    // then
+    assertThat(rowSums[0]).isEqualTo(3);
+    assertThat(rowSums[1]).isEqualTo(4);
+  }
+
+  @Test
+  public void testCountColumnSums() {
+    // given
+    Integer[] columnSums = underTest.countColumnSums();
+
+    // then
+    assertThat(columnSums[0]).isEqualTo(1);
+    assertThat(columnSums[1]).isEqualTo(2);
+    assertThat(columnSums[2]).isEqualTo(4);
+  }
 }
--- src/test/java/spockpv/GameTest.java
+++ src/test/java/spockpv/GameTest.java
@@ -8,76 +8,70 @@ import org.junit.Test;
 
 public class GameTest {
 
-    Character[][] initialBoardState = {
-        {'1', '0', '2'},
-        {'2', '1', '3'},
-        {'3', '2', '1'}
-    };
-    Board board = new Board(initialBoardState);
-    Game game = new Game(board);
+  Character[][] initialBoardState = {
+      {'1', '0', '2'}, {'2', '1', '3'}, {'3', '2', '1'}};
+  Board board = new Board(initialBoardState);
+  Game game = new Game(board);
 
-    @Test
-    public void testGetCurrentBoardReturnsProperBoard() {
-        // given
-        Board expected = board;
+  @Test
+  public void testGetCurrentBoardReturnsProperBoard() {
+    // given
+    Board expected = board;
 
-        // when
-        Board actual = game.getCurrentBoard();
-        
-        // then
-        assertSame(expected, actual);
-    }
+    // when
+    Board actual = game.getCurrentBoard();
 
-    @Test
-    public void testGameIsWon() {
-        // given
-        Character[][] wonBoardState = {
-            {'1', '3', '2'},
-            {'2', '1', '3'},
-            {'3', '2', '1'}
-        };
-        Board wonBoard = new Board(wonBoardState);
-        Game wonGame = new Game(wonBoard);
-        
-        // when
-        boolean isWon = wonGame.isWon();
+    // then
+    assertSame(expected, actual);
+  }
 
-        // then
-        assertTrue(isWon);
-    }
+  @Test
+  public void testGameIsWon() {
+    // given
+    Character[][] wonBoardState = {
+        {'1', '3', '2'}, {'2', '1', '3'}, {'3', '2', '1'}};
+    Board wonBoard = new Board(wonBoardState);
+    Game wonGame = new Game(wonBoard);
 
-    @Test
-    public void testGameIsNotWon() {
-        // when
-        boolean isWon = game.isWon();
+    // when
+    boolean isWon = wonGame.isWon();
 
-        // then
-        assertFalse(isWon);
-    }
+    // then
+    assertTrue(isWon);
+  }
 
-    @Test
-    public void testTryPutValueFails() {
-        // given
-        Coordinates coordinates = new Coordinates(0, 0);
-        Character value = 'x';
+  @Test
+  public void testGameIsNotWon() {
+    // when
+    boolean isWon = game.isWon();
 
-        // when
-        boolean result = game.tryPutValue(coordinates, value);
+    // then
+    assertFalse(isWon);
+  }
 
-        // then
-        assertFalse(result);
-    }
+  @Test
+  public void testTryPutValueFails() {
+    // given
+    Coordinates coordinates = new Coordinates(0, 0);
+    Character value = 'x';
 
-    @Test
-    public void testTryPutValueSucceeds() {
-        // given
-        Coordinates coordinates = new Coordinates(0, 1);
-        Character value = 'x';
+    // when
+    boolean result = game.tryPutValue(coordinates, value);
 
-        // when
-        boolean result = game.tryPutValue(coordinates, value);
+    // then
+    assertFalse(result);
+  }
 
-        // then
-        assertTrue(result);
-    }
+  @Test
+  public void testTryPutValueSucceeds() {
+    // given
+    Coordinates coordinates = new Coordinates(0, 1);
+    Character value = 'x';
+
+    // when
+    boolean result = game.tryPutValue(coordinates, value);
+
+    // then
+    assertTrue(result);
+  }
 }
