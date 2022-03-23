4c295f36ee0153d2548b2cd4ef697fbb98752b9f
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 22:41:17 +0100

    Docs: Document UserInterface

--- src/main/java/spockpv/UserInterface.java
+++ src/main/java/spockpv/UserInterface.java
@@ -1,9 +1,40 @@
 package spockpv;
 
+/**
+ * Class reresenting user interdace. Has to implement all following methods in
+ * order for game to work properly.
+ *
+ * @author  Filip Wiśniewski
+ */
 public interface UserInterface {
-    public Coordinates getCoords();
-    public Character getValue();
-    public void draw(Board board);
-    public void promptUnmodifiableCell();
-    public void promptGameWon(Integer points);
+  /**
+   * Gets coordinates from user
+   * @return coordinates provided by user input
+   */
+  public Coordinates
+  getCoords(); // TODO user interface probably should not create objects, just
+               // return input replace with getX, getY
+
+  /**
+   * Gets character from user
+   * @return character provided by user input
+   */
+  public Character getValue();
+
+  /**
+   * Draws game board
+   * @param board
+   */
+  public void draw(Board board);
+
+  /**
+   * Informs user that cell cannot be mofified
+   */
+  public void promptUnmodifiableCell();
+
+  /**
+   * Tells user that they won. Also prints number of points collected
+   * @param points
+   */
+  public void promptGameWon(Integer points);
 }
