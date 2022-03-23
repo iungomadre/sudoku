5f514db22c1c47a583c610c23e28787acdc6ebd3
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 22:36:31 +0100

    Docs: Document Game

--- src/main/java/spockpv/Game.java
+++ src/main/java/spockpv/Game.java
@@ -1,51 +1,72 @@
 package spockpv;
 
+/**
+ * Class representing game logic
+ * 
+ * @author  Filip Wiśniewski
+ */
 public class Game {
 
-    final Board initialBoard_;
-    final Board currentBoard_;
-    
-    public Game(Board board) {
-        currentBoard_ = board;
-        initialBoard_ = (Board)board.clone();
-    }
+  final Board initialBoard_; // initial state of the board
+  final Board currentBoard_; // current state of the board
 
-    public Board getCurrentBoard() {
-        return currentBoard_;
-    }
+  /**
+   * Parameter constructor
+   * @param board board to play with
+   */
+  public Game(Board board) {
+    currentBoard_ = board;
+    initialBoard_ = (Board)board.clone();
+  }
 
-    public boolean isWon() {
-        // game won if sum in every row and column is equal (1+...+n)
-        
-        Integer expectedRowSum = countSumN(currentBoard_.width());
-        Integer expectedColumnSum = countSumN(currentBoard_.height());
+  /**
+   * @return current game board (using pointer)
+   */
+  public Board getCurrentBoard() { return currentBoard_; }
 
-        for (Integer rowSum : currentBoard_.countRowSums()) {
-            if (rowSum != expectedRowSum) {
-                return false;
-            }
-        }
-        for (Integer columnSum : currentBoard_.countColumnSums()) {
-            if (columnSum != expectedColumnSum) {
-                return false;
-            }
-        }
-        return true;
-    }
+  /**
+   * Gane is won, when sum in every row and column is equal (1+2+...+n), where n
+   * is width (or length) of the board
+   * @return true, if game won
+   */
+  public boolean isWon() {
 
-    private Integer countSumN(Integer N) {
-        Integer sum = 0;
-        for (Integer i = 1; i <= N; i++) {
-            sum = sum + i;
-        }
-        return sum;
-    }
+    Integer expectedRowSum = countSumN(currentBoard_.width());
+    Integer expectedColumnSum = countSumN(currentBoard_.height());
 
-    public boolean tryPutValue(Coordinates coordinates, Character value) {
-        if (initialBoard_.getCellValue(coordinates) != '0') {
-            return false;
-        }
-        currentBoard_.setCellValue(value, coordinates);
-        return true;
+    for (Integer rowSum : currentBoard_.countRowSums()) {
+      if (rowSum != expectedRowSum) {
+        return false;
+      }
     }
+    for (Integer columnSum : currentBoard_.countColumnSums()) {
+      if (columnSum != expectedColumnSum) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private Integer countSumN(Integer n) {
+    // counts sum of (1, ..., n)
+    Integer sum = 0;
+    for (Integer i = 1; i <= n; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+
+  /**
+   * Tries putting value to certain coordinates
+   * @param coordinates
+   * @param value
+   * @return true, if has succeed
+   */
+  public boolean tryPutValue(Coordinates coordinates, Character value) {
+    if (initialBoard_.getCellValue(coordinates) != '0') {
+      return false;
+    }
+    currentBoard_.setCellValue(value, coordinates);
+    return true;
+  }
 }
