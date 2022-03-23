35632b73d1152ecee7ae125f7981179d72a28c35
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 18:51:59 +0100

    Docs: Document App

--- src/main/java/spockpv/App.java
+++ src/main/java/spockpv/App.java
@@ -1,30 +1,46 @@
 package spockpv;
-
+/**
+ * <h1>Sudoku</h1>
+ * Sudoku is a simple console game that displays board on standard output
+ *
+ * @author  Filip Wiśniewski
+ */
 public class App {
 
-    public static void main(String[] args) throws CloneNotSupportedException {
+  /**
+   * Main method that runs game
+   *
+   * @param args unused
+   * @return nothing
+   */
+  public static void main(String[] args) {
 
-        BoardProvider boardProvider = new BasicBoardProvider();
-        UserInterface ui = new ConsoleUserInterface();
-        Board board = boardProvider.createBoard();
+    BoardProvider boardProvider = new BasicBoardProvider();
+    UserInterface ui = new ConsoleUserInterface();
+    Board board = boardProvider.createBoard();
 
-        Game game = new Game(board);
-        play(game, ui);
+    Game game = new Game(board);
+    play(game, ui);
+  }
+
+  /**
+   * Runs whole game synchronously until won
+   *
+   * @param game object to be played
+   * @param ui user interface
+   */
+  private static void play(Game game, UserInterface ui) {
+    Integer movesNumber = 0;
+
+    while (!game.isWon()) {
+      movesNumber++;
+
+      ui.draw(game.getCurrentBoard());
+
+      if (!game.tryPutValue(ui.getCoords(), ui.getValue())) {
+        ui.promptUnmodifiableCell();
+      }
     }
-
-    private static void play(Game game, UserInterface ui) {
-        Integer movesNumber = 0;
-
-        while (!game.isWon()) {
-            movesNumber++;
-
-            ui.draw(game.getCurrentBoard());
-
-            if (!game.tryPutValue(ui.getCoords(), ui.getValue())) {
-                ui.promptUnmodifiableCell();
-            }
-        }
-        ui.promptGameWon(movesNumber);
-    }
+    ui.promptGameWon(movesNumber);
+  }
 }
-
