d9ce048829a704d0d450e4a1bb9b553f6464817d
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-03-23 17:56:36 +0100

    Refactor: Clean Board

--- src/main/java/spockpv/Board.java
+++ src/main/java/spockpv/Board.java
@@ -1,7 +1,7 @@
 package spockpv;
 
 public class Board implements Cloneable {
-    /* Represents Sudoku board and its state */
+    /* Represents Sudoku board state */
 
     private Character[][] boardState_;
 
@@ -24,35 +24,7 @@ public class Board implements Cloneable {
     public Integer height() {
         return boardState_[0].length;
     }
-
-    public Integer[] countRowSums() {
-        Integer[] sums = new Integer[this.height()];
-        Integer sum;
-
-        for (int y = 0; y < this.height(); y++) {
-            sum = 0;
-            for (int x = 0; x < this.width(); x++) {
-                sum = sum + Character.getNumericValue(boardState_[x][y]);
-            }
-            sums[y] = sum;
-        }
-        return sums;
-    }
-
-    public Integer[] countColumnSums() {
-        Integer[] sums = new Integer[this.width()];
-        Integer sum;
-
-        for (int x = 0; x < this.width(); x++) {
-            sum = 0;
-            for (int y = 0; y < this.height(); y++) {
-                sum = sum + Character.getNumericValue(boardState_[x][y]);
-            }
-            sums[x] = sum;
-        }
-        return sums;
-    }
-
+    
     public boolean equals(Object o) {
         if (o == this) {
             return true;
@@ -60,7 +32,7 @@ public class Board implements Cloneable {
         if (!(o instanceof Board)) {
             return false;
         }
-
+        
         Board compared = (Board)o;
         for (int i = 0; i < compared.boardState_.length; i++) {
             for (int j = 0; j < compared.boardState_[i].length; j++) {
@@ -71,18 +43,48 @@ public class Board implements Cloneable {
         }
         return true;
     }
-
+    
     @Override
     public Object clone() {
-
+        
         Character[][] newBoardState = new Character[this.boardState_.length][this.boardState_[0].length];
-
+        
         for (Integer i = 0; i < this.boardState_.length; i++) {
             for (Integer j = 0; j < this.boardState_[i].length; j++) {
                 newBoardState[i][j] = this.boardState_[i][j];
             }
         }
-
+        
         return new Board(newBoardState);
     }
+    
+        // counts sum of numbers in every row
+        public Integer[] countRowSums() {
+            Integer[] sums = new Integer[this.height()];
+            Integer sum;
+    
+            for (int y = 0; y < this.height(); y++) {
+                sum = 0;
+                for (int x = 0; x < this.width(); x++) {
+                    sum = sum + Character.getNumericValue(boardState_[x][y]);
+                }
+                sums[y] = sum;
+            }
+            return sums;
+        }
+    
+        // counts sum of numbers in every column
+        public Integer[] countColumnSums() {
+            Integer[] sums = new Integer[this.width()];
+            Integer sum;
+    
+            for (int x = 0; x < this.width(); x++) {
+                sum = 0;
+                for (int y = 0; y < this.height(); y++) {
+                    sum = sum + Character.getNumericValue(boardState_[x][y]);
+                }
+                sums[x] = sum;
+            }
+            return sums;
+        }
 }
